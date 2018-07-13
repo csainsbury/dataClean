ebe6ca11688b6721f80a188373267fe55f10c2bb
chris sainsbury <chris@sainsbury.im>
2018-07-13 09:24:32 +0100

    .

--- splineInterpolate.R
+++ splineInterpolate.R
@@ -89,7 +89,7 @@ cleanBMIData <- read.csv("~/R/GlCoSy/SD_workingSource/BMIsetDTclean.csv", sep=",
   bmiLimited <- data.frame(cleanBMIData$LinkId, cleanBMIData$bmiNumeric, cleanBMIData$dateplustime1); colnames(bmiLimited) <- c("LinkId", "numericValue", "dateplustime1")
   bmiLimited <- data.table(bmiLimited)
 
-splineInterpol(hba1cLimited, "2012-01-01", "2017-01-01", 60, 8, 20, 200, "hba1c_5y")
+splineInterpol(hba1cLimited, "2011-01-01", "2017-01-01", 120, 8, 20, 200, "hba1c_6y")
 splineInterpol(sbpLimited, "2012-01-01", "2017-01-01", 60, 8, 80, 240, "sbp_5y")
 splineInterpol(dbpLimited, "2012-01-01", "2017-01-01", 60, 8, 40, 160, "dbp_5y")
 splineInterpol(bmiLimited, "2012-01-01", "2017-01-01", 60, 6, 16, 100, "bmi_5y")
